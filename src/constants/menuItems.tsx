4026534b59f16f36daaa2ad49480384dbf056655
Jérémy <jeremy.vaneste@free.fr>
2023-03-21 17:29:58 +0100

    create all items of siderbar menu

--- src/constants/menuItems.tsx
+++ src/constants/menuItems.tsx
@@ -1,4 +1,6 @@
-import { HomeOutlined, WalletOutlined } from "@ant-design/icons";
+import { AiOutlineHome, AiOutlineBook } from "react-icons/ai";
+import { CgCardHearts } from "react-icons/cg";
+import { HiOutlineBuildingStorefront } from "react-icons/hi2";
 
 type MenuItemType = {
   label: string;
@@ -10,21 +12,21 @@ export const items: MenuItemType[] = [
   {
     label: "Home",
     key: "1",
-    icon: <HomeOutlined />,
+    icon: <AiOutlineHome />,
   },
   {
     label: "Collection",
     key: "2",
-    icon: <WalletOutlined />,
+    icon: <AiOutlineBook />,
   },
-  //   {
-  //     label: "Cards",
-  //     key: "3",
-  //     icon: <TeamOutlined />,
-  //   },
-  //   {
-  //     label: "Dashboard",
-  //     key: "4",
-  //     icon: <TeamOutlined />,
-  //   },
+  {
+    label: "Cards",
+    key: "3",
+    icon: <CgCardHearts />,
+  },
+  {
+    label: "Marketplace",
+    key: "4",
+    icon: <HiOutlineBuildingStorefront />,
+  },
 ];
