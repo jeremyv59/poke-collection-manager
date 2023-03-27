ca8ea87a47cef44c66fa4e67a298d24b1fda8dc2
Jérémy <jeremy.vaneste@free.fr>
2023-03-27 13:50:20 +0200

    delete AppContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,11 @@ import Sidebar from "./components/sidebar/Sidebar";
 import GlobalStyles from "./styles/GlobalStyles";
 import { theme } from "./styles/Theme";
 
-const AppContainer = styled.div`
-  background-color: red;
-  min-height: 100vh;
-`;
-
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <AppContainer>
-        <Sidebar />
-      </AppContainer>
-      {/* <Homepage /> */}
-      {/* <p>Salut les copains</p> */}
+      <Sidebar />
     </ThemeProvider>
   );
 };
