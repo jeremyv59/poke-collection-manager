9894a73a11be09bc0fcb0ec6b0ad904b4ee01b3b
Jérémy <jeremy.vaneste@free.fr>
2023-03-27 13:58:10 +0200

    rename all pages files entry points (index.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import styled, { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import Sidebar from "./components/sidebar/Sidebar";
 import GlobalStyles from "./styles/GlobalStyles";
 import { theme } from "./styles/Theme";
+// import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Sidebar />
+      {/* <BrowserRouter></BrowserRouter> */}
     </ThemeProvider>
   );
 };
