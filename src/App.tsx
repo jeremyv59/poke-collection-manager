cd4122b187db028513730f3c163b4cac7aa19537
Jérémy <jeremy.vaneste@free.fr>
2023-03-27 14:12:12 +0200

    implement router component into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { ThemeProvider } from "styled-components";
 import Sidebar from "./components/sidebar/Sidebar";
 import GlobalStyles from "./styles/GlobalStyles";
 import { theme } from "./styles/Theme";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./pages/router";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Sidebar />
-      {/* <BrowserRouter></BrowserRouter> */}
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
