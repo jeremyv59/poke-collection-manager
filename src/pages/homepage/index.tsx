9cf2b488ef29dc94c08896d20057edd03bc64764
Jérémy <jeremy.vaneste@free.fr>
2023-03-29 17:39:05 +0200

    Header design implementation

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -2,41 +2,45 @@ import React from "react";
 import {
   CustomHeader,
   HomepageLayout,
+  IconContainer,
   MainContainer,
+  OptionsContainer,
   RightContainer,
+  UserContainer,
+  UserName,
+  UserDescContainer,
+  Status,
+  ProfileIcon,
 } from "./home_styled";
+import { FiSettings } from "react-icons/fi";
+import { IoNotificationsOutline } from "react-icons/io5";
 
 const Homepage = () => {
   return (
     <HomepageLayout>
       <CustomHeader>
-        <h1>Home</h1>
-        <text>Hello Jérémy, welcome back !</text>
+        <div>
+          <h1>Home</h1>
+          <text>Hello Jérémy, welcome back !</text>
+        </div>
+        <OptionsContainer>
+          <IconContainer>
+            <FiSettings className="icon" size={"1.25rem"} />
+          </IconContainer>
+          <IconContainer>
+            <IoNotificationsOutline className="icon" size={"1.25rem"} />
+          </IconContainer>
+          <UserContainer>
+            <UserDescContainer>
+              <UserName>Jérémy Vaneste</UserName>
+              <Status>User</Status>
+            </UserDescContainer>
+            <ProfileIcon />
+          </UserContainer>
+        </OptionsContainer>
       </CustomHeader>
-      <MainContainer>
-        Main Container Main Container Main Container Main Container Main
-        Container Main Container Main ContainerMain Container
-      </MainContainer>
-
+      <MainContainer>Main Container</MainContainer>
       <RightContainer>Right Container</RightContainer>
-      {/* <Layout className="site-layout">
-        <Header style={{ padding: 0, background: "blue", width: "70%" }} />
-        <Content style={{ margin: "0 16px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: "red",
-            }}
-          >
-            Bill is a cat.
-          </div>
-        </Content>
-      </Layout> */}
     </HomepageLayout>
   );
 };
