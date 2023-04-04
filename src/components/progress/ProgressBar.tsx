a6b389f1c468162d5f1d3726d0acdb046de29b8f
JeremyV <jeremy.vaneste@free.fr>
2023-04-04 16:34:36 +0200

    add function to manage stroke color of progressbar

--- src/components/progress/ProgressBar.tsx
+++ src/components/progress/ProgressBar.tsx
@@ -1,16 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { AntProgress } from "./progress_styled";
 import { checkPercentage } from "../../utils/checkPercentage";
 
 const ProgressBar: React.FC = () => {
   const [percentage, setPercentage] = useState<number>(0);
+  const [strokeColor, setStrokeColor] = useState<string | undefined>();
+
+  const updateStrokeColor = () => {
+    if (percentage <= 25) {
+      return "red";
+    } else if (percentage <= 50) {
+      return "orange";
+    } else if (percentage <= 75) {
+      return "yellow";
+    } else {
+      return "green";
+    }
+  };
 
   useEffect(() => {
-    let percentagResult = checkPercentage(50, 100);
+    let percentagResult = checkPercentage(90, 100);
     setPercentage(percentagResult);
   }, []);
 
-  return <AntProgress percent={percentage}></AntProgress>;
+  useEffect(() => {
+    let colorRes = updateStrokeColor();
+    setStrokeColor(colorRes);
+  }, [percentage]);
+
+  return (
+    <AntProgress strokeColor={strokeColor} percent={percentage}></AntProgress>
+  );
 };
 
 export default ProgressBar;
