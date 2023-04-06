121129624489096860a34826eae1881e89d61c30
JeremyV <jeremy.vaneste@free.fr>
2023-04-06 14:48:05 +0200

    Card to display total of cards in my collection => ok

--- src/pages/homepage/home_styled.tsx
+++ src/pages/homepage/home_styled.tsx
@@ -47,6 +47,53 @@ export const RightContainer = styled.div`
 
   @media ${device.tablet} {
     width: 30%;
-    padding-top: 150px;
+    padding-top: 100px;
   }
+`;
+
+export const ContentContainer = styled.div`
+  padding: 0rem 2rem;
+  margin-top: 1rem;
+`;
+
+export const TotalCards = styled.div`
+  background: ${({ theme }) => theme.colors.darkSecondary};
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 1.875rem;
+  padding: 1.875rem 0rem;
+
+  .ant-btn {
+    display: flex;
+    background: brown;
+    align-items: center;
+    justify-content: space-between;
+    width: 60%;
+    flex-direction: row-reverse;
+    background-color: #5464f7;
+    color: white;
+  }
+`;
+
+export const TotalTitle = styled.h6`
+  font-size: 1rem;
+  color: white;
+  font-weight: 500;
+`;
+
+export const TotalCount = styled.h6`
+  color: ${({ theme }) => theme.colors.purple};
+  font-weight: 700;
+  font-size: 2.75rem;
+  line-height: 60px;
+`;
+
+export const DateInfo = styled.small`
+  color: ${({ theme }) => theme.colors.gray};
+  font-weight: 500;
+  font-size: 0.875rem;
+  margin-bottom: 1.25rem;
 `;
--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,14 +1,21 @@
 import {
+  ContentContainer,
+  DateInfo,
   HomepageLayout,
   ListContainer,
   MainCardContainer,
   MainContainer,
   RightContainer,
+  TotalCards,
+  TotalCount,
+  TotalTitle,
 } from "./home_styled";
 import Header from "../../components/header/Header";
 import CustomCard from "../../components/card/CustomCard";
 import ExpansionsList from "../../components/expansionsList/ExpansionsList";
 import { expansionsData } from "../../constants/expansionsData";
+import CustomBtn from "../../components/button/CustomBtn";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 const Homepage = () => {
   return (
@@ -22,7 +29,20 @@ const Homepage = () => {
           <ExpansionsList listTitle="Latest expansions" data={expansionsData} />
         </ListContainer>
       </MainContainer>
-      <RightContainer>Right Container</RightContainer>
+      <RightContainer>
+        <ContentContainer>
+          <TotalCards>
+            <TotalTitle>Your total of cards :</TotalTitle>
+            <TotalCount>3875</TotalCount>
+            <DateInfo>06 avr 2023 at 14:18 PM</DateInfo>
+            <CustomBtn
+              icon={<ArrowRightOutlined />}
+              btnTitle="My collection"
+              btnType="text"
+            />
+          </TotalCards>
+        </ContentContainer>
+      </RightContainer>
     </HomepageLayout>
   );
 };
